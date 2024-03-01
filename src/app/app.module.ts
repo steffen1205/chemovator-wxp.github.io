cfa278239c31668582db7967f9ac202d2d54fdb2
Steffen Hofbauer <steffen.hofbauer@basf.com>
2024-03-01 18:55:10 +0100

    adding landing page && nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // primeng
 import { ButtonModule } from 'primeng/button';
+import { RatingModule } from 'primeng/rating';
+
 
 // own components
 import { DialogComponent } from './components/dialog/dialog.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DialogComponent,
-    NavbarComponent
+    NavbarComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     ButtonModule,
+    RatingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
